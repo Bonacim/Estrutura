303da00781f4ca53bb2221034db29a850e0325f7
Bonacim <38168038+Bonacim@users.noreply.github.com>
2018-04-08 21:14:29 -0300

    Add files via upload
    Adicionado SDL

--- estrutura_sdl.h
+++ estrutura_sdl.h
@@ -1,1 +1,23 @@
-//SDL
+#include "estrutura.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 1920;
+const int SCREEN_HEIGHT = 1036;
+
+const int SDL_OFFSET = 20;
+const double SDL_ESCALA = 100;
+
+//Starts up SDL and creates window
+bool init();
+
+//Frees media and shuts down SDL
+void close();
+
+void create_window();
+
+void render_window(bool *quit, Estrutura* obj);
+
+int arredonda (double x);
